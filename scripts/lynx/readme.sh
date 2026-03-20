02d6a0b84c5a9d742e4c9ce687e3b309c9168bf1
zuxinrui <zuxinrui95@gmail.com>
2026-03-20 11:29:00 +0100

    vu

--- scripts/lynx/readme.sh
+++ scripts/lynx/readme.sh
@@ -62,6 +62,6 @@ python scripts/reinforcement_learning/rsl_rl/play.py --task=Isaac-Push-Cube-Lynx
 # Interactive debug command (GUI, fewer envs for smooth viewport):
 python scripts/reinforcement_learning/rsl_rl/play.py --task=Isaac-Push-Cube-Lynx-Play-v0 --num_envs 8 --load_run /home/zuxinrui/IsaacLab/logs/rsl_rl/lynx_push/2026-03-18_23-07-04 --checkpoint /home/zuxinrui/IsaacLab/logs/rsl_rl/lynx_push/2026-03-18_23-07-04/model_1499.pt
 
-python scripts/reinforcement_learning/rsl_rl/play.py --task=Isaac-Ball-In-Cup-Lynx-Play-v0 --num_envs 8 --load_run /home/zuxinrui/IsaacLab/logs/rsl_rl/lynx_ball_in_cup/2026-03-19_13-46-12 --checkpoint /home/zuxinrui/IsaacLab/logs/rsl_rl/lynx_ball_in_cup/2026-03-19_13-46-12/model_100.pt
+python scripts/reinforcement_learning/rsl_rl/play.py --task=Isaac-Ball-In-Cup-Lynx-Play-v1 --num_envs 64 --load_run /home/zuxinrui/IsaacLab/logs/rsl_rl/Lynx_ball_in_cup/2026-03-20_01-52-04 --checkpoint /home/zuxinrui/IsaacLab/logs/rsl_rl/Lynx_ball_in_cup/2026-03-20_01-52-04/model_550.pt
 
 
